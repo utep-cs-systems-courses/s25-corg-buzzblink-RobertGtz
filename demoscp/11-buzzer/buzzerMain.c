b25de83aaa866a03b354772050cb820aab674896
RobertGtz <rgutierrez25@miners.utep.edu>
2025-03-24 00:53:42 -0600

    I save my project on 11-buzzer, not done yet

--- demoscp/11-buzzer/buzzerMain.c
+++ demoscp/11-buzzer/buzzerMain.c
@@ -3,16 +3,16 @@
 #include "buzzer.h"
 
 #define BUTTOMS (BIT0 | BIT1 | BIT2 | BIT3)
-
 #define LED_RED BIT0               // P1.0
-
 #define LED_GREEN BIT6             // P1.6
-
-#define LEDS (BIT0 | BIT6)
+#define LEDS (LED_RED | LED_GREEN)
 
 void led_init();
-
 void led_update();
+void state_advance();
+void startNote();
+void buttom_init();
+void switch_interrupt_handler();
 
 
 int melody[] = {
@@ -23,51 +23,29 @@ int melody[] = {
 };
 
 int currentNote = 0;
-
-
-extern unsigned char red_on, green_on;
-
-extern unsigned char led_changed;
-
-
 unsigned char red_on = 1, green_on = 0;
-
 unsigned char led_changed = 0;
-
+volatile int isPlaying = 0;
 
 
 static char redVal[] = {0, LED_RED}, greenVal[] = {0, LED_GREEN};
 
-
-
-
-
-void led_init()
-
-{
+void led_init() {
 
   P1DIR |= LEDS;// bits attached to leds are output
-
   led_changed = 1;
-
   led_update();
-
+  
 }
 
 
 
-void led_update()
-
-{
-
+void led_update() {
+  
   if (led_changed) {
-
     char ledFlags = redVal[red_on] | greenVal[green_on];
 
-
-
     P1OUT &= (0xff^LEDS) | ledFlags; // clear bit for off leds
-
     P1OUT |= ledFlags;     // set bit for on leds
 
     led_changed = 0;
@@ -76,35 +54,19 @@ void led_update()
 
 }
 
-char toggle_red()/* always toggle! */
-
-{
-
-  static char state = 0;
-  if (!(currentNote & 1)){
-    red_on ^= 1;
-
-    state = 1;
-  }
+char toggle_red() {
   
-  return 1;/* always changes an led */
-
+  red_on ^= 1;
+  return 1;
+  
 }
 
-
-
-char toggle_green()/* only toggle green if red is on!  */
-
-{
+char toggle_green() {
 
   char changed = 0;
-
-  if (!(currentNote & 1)) {
-
+  if (red_on) {
     green_on ^= 1;
-
     changed = 1;
-
   }
 
   return changed;
@@ -115,35 +77,27 @@ char toggle_green()/* only toggle green if red is on!  */
 
 
 
-void state_advance()/* alternate between toggling red & green */
-
-{
+void state_advance() {
 
   char changed = 0;
-
-
-
   static enum {R=0, G=1} color = G;
-  if (currentNote < sizeof(melody) / sizeof(melody[0])) {
+  
+  if (isPlaying && currentNote < sizeof(melody) / sizeof(melody[0])) {
     switch (color) {
-
     case R: changed = toggle_red(); color = G; break;
-
     case G: changed = toggle_green(); color = R; break;
 
     }
   }
 
-
   led_changed = changed;
-
   led_update();
 
 }
 
 void startNote() {
   
-  if (currentNote < sizeof(melody) / sizeof(melody[0])) {
+  if (isPlaying && currentNote < sizeof(melody) / sizeof(melody[0])) {
     buzzer_set_period(melody[currentNote] == 0 ? 0 : 2000000 / melody[currentNote]);
     currentNote++; // Move to the next note
 
@@ -153,17 +107,34 @@ void startNote() {
   }
   
 }
+
+
+/*
+void
+switch_interrupt_handler(){
+  
+  char p2val = P2IN & BUTTOMS;
+  if(!(p2val & BIT0)){
+    buzzer_init();
+    led_init();
+    startNote();
+  }
+}
+*/
+
 void __interrupt_vec(WDT_VECTOR) WDT(){
   static char delay = 0;
   static char currentBlink = 0;
-  if (++delay == 100){
-    currentNote++;
-    startNote();
-    delay = 0;
-  }
-  if (++currentBlink >= 50){
-    state_advance();
-    currentBlink = 0;
+  if(isPlaying){
+    if (++delay == 100){
+      startNote();
+      delay = 0;
+    }
+    
+    if (++currentBlink >= 50){
+      state_advance();
+      currentBlink = 0;
+    }
   }
 }
 
@@ -171,22 +142,37 @@ void buttom_init(){
   P2REN |= BUTTOMS;
   P2IE |= BUTTOMS;
   P2OUT |= BUTTOMS;
-  P2DIR &= ~BUTTMOS;
+  P2DIR &= ~BUTTOMS;
+//P2IFG &= ~BUTTOMS;
 }
 
-void switch_inter
+void __interrupt_vec(PORT2_VECTOR) Port2_ISR() {
+
+  if (P2IFG & BUTTOMS) { // Check if any button triggered the interrupt
+    // Debounce delay
+    __delay_cycles(2000); // 1 ms delay at 2 MHz
+    
+    if ((P2IN & BUTTOMS) != BUTTOMS) { // Check if any button is still pressed
+      // Toggle the melody playback
+      isPlaying = !isPlaying;
+      if (!isPlaying) {
+	buzzer_set_period(0);
+	P1OUT &= ~LEDS;
+	
+      }
+    }
+    P2IFG &= ~BUTTOMS; // Clear the interrupt flag
+  }
+
+}
+
 
 int main() {
   configureClocks();
-  P1DIR |= LEDS;
-  P1OUT &= ~LEDS;
+  enableWDTInterrupts();
+  led_init();
+  buzzer_init();
   buttom_init();
   
-  buzzer_init();
-  
-  led_init();
-  
-  startNote();
-  enableWDTInterrupts();
   or_sr(0x18);          // CPU off, GIE on
 }
